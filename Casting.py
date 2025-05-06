95ab3e79a6e7567e27c8d68c23ab9540c29772d2
kazimehedihasan1 <mehedihasanarabi00@gmail.com>
2025-05-06 16:04:19 +0600

    learn Casting

---
+++ Casting.py
@@ -0,0 +1,12 @@
+# TYPE CASTING 
+
+#examples --->
+
+cast = "5" # i want to cast. of int value.
+print(type(cast))
+print(cast)
+print(float(cast))
+
+castOne = 8.9
+print(castOne)
+print(int(castOne))
