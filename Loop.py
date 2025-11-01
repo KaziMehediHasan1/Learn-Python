f7f46801657fdf5dcd78cba6c99d1e05857851dc
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-01 12:56:16 +0600

    completed loop

---
+++ Loop.py
@@ -0,0 +1,41 @@
+thisDict ={
+    "name":"kazi mehedi hasan",
+    "age":24,
+    "isStudent":False,
+    "FavouritesColors":["red","blue","white"]
+}
+
+# just showing keys
+"""for x in thisDict:
+    save = x
+    if "age" in save:
+         print("have this");
+    else:
+         print("nothing yet")"""
+
+# line by line shows all values
+"""
+for x in thisDict:
+    save = x
+    print(thisDict[save])"""
+
+#  line by line shows all values
+"""for x in thisDict.values():
+    print(x,"values")
+        """
+
+# both get using items() method
+"""for x,y in thisDict.items():
+    print("key -->", x,  "value -->", y)"""
+
+
+
+
+""" copy dict using many ways """
+
+"""copyOne = dict(thisDict);
+print(copyOne,"copy dict")"""
+
+copyTwo = thisDict.copy();
+print(copyTwo,"copy way")
+   
