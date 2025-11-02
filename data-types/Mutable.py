255d37d099ef521b8d55c26b85280729b941a5b6
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-02 21:32:31 +0600

    completed mutable data type

---
+++ data-types/Mutable.py
@@ -0,0 +1,27 @@
+# list, dict, set
+
+
+# List
+"""a = [24,"kazi mehedi",3.66,"diploma"]
+print(type(a))
+a[2]="Mehedi Hasan"
+print(id(a))
+print(id(a))"""
+
+
+#  Dict
+"""b = {
+    "gradution":"diploma",
+    "age":24,
+    "name":"mehedi",
+    "family":["3 members",30,44,{"kazi":"all"}]
+}
+print(type(b["family"][3]))"""
+
+
+# Set
+c = {"kazi","marride","nai",34,5.5}
+for item in c:
+    print(item)
+c_list = list(c)
+print(c_list[1])
