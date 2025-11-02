2f44bec1941694f67354ad9bcd15b7633e73c289
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-02 21:20:45 +0600

    completed immutable data types

---
+++ data-types/Immutable.py
@@ -0,0 +1,25 @@
+# ing, float, str, tuple, frozenset
+
+a = 24
+b =3.66
+c = "cgpa - 3.66"
+d = ["kazi mehedi",24,"diplma",3.66]
+e = frozenset([a,3,5])
+
+
+"""x=id(a)
+print(id(a))
+a = "abdc"
+y=id(a)
+print(id(a))
+print(x == y)"""
+
+"""first_id = id(a)
+a = 25
+second_id = id(a)
+
+print(first_id)
+print(second_id)"""
+
+# d = ["hello world"]
+print()
