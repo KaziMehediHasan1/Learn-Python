4d4d717320ee06bf49f3170763d6643b1bfc0dc8
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-04 19:36:11 +0600

    Strint_Indexing_Slicing.py

---
+++ Strint_Indexing_Slicing.py
@@ -0,0 +1,15 @@
+# Position Indexing
+
+text="Hello World"
+
+# print(text[0]) 
+# print(text[4])
+
+# Negative Indexing
+
+# print(text[-1])
+# print(text[-6])
+
+# Slicing
+
+print(text[6:])   # Hello
