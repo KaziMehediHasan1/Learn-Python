e21c08b03744112efc06bce057f4652be4c61b7c
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-04 19:06:30 +0600

    string manupulation completed

---
+++ String_Manufulation.py
@@ -0,0 +1,13 @@
+name = "Kazi Mehedi Hasan, 'AI Enthusiast', 'Python Developer'"
+print("Original String:",name),
+
+# multiply the string
+names = """
+    Kazi Mehedi Hasan is an :-
+   'AI Enthusiast',
+    'Python Developer',
+    'Web Developer',
+    'Data Scientist',
+"""
+
+print("Multiplied String:",names)
