5c6ad234c201122a5545e952691e887087b82a2e
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-08 22:25:59 +0600

    Number_Math complete

---
+++ Number_Math.py
@@ -0,0 +1,12 @@
+a = 10
+b =20.4
+c = 30j
+
+print("Addition", a + b)
+print("subtraction",20 - a)
+print("Multiplication",a * b)
+print("Division", 40 / 2)
+print("Floor division", 40 // 3)
+print("Modulus", 40 % 3)
+print("Power", 3 ** 3 )
+
