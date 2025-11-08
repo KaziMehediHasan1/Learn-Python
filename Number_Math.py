aff02b9e8d0cd137ab015ae14933c282098620be
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-08 22:52:43 +0600

    completed all number operations!

--- Number_Math.py
+++ Number_Math.py
@@ -2,11 +2,41 @@ a = 10
 b =20.4
 c = 30j
 
-print("Addition", a + b)
+"""print("Addition", a + b)
 print("subtraction",20 - a)
 print("Multiplication",a * b)
 print("Division", 40 / 2)
 print("Floor division", 40 // 3)
 print("Modulus", 40 % 3)
-print("Power", 3 ** 3 )
+print("Power", 3 ** 3 )"""
 
+
+""" Comparison Operator """
+
+"""itsEqual = a == b
+print(itsEqual,"equal")
+itsNotEqual=a != b
+print(itsNotEqual,"itsNotEqual")
+itsGreaterthan = 20 > 10
+print(itsGreaterthan,"itsGreaterthan")
+itsLessthan = 20 < 10
+print(itsLessthan,"itsLessthan")
+itsGreaterorequal = 5 >= 5
+print(itsGreaterorequal,"itsGreaterorequal")
+itsLessOrequal = 5<=10
+print(itsLessOrequal,"itsLessOrequal")"""
+
+# math function -- 
+"""import math
+print(math.sqrt(16),"sqrt")
+print(math.pow(4,3))
+print(math.factorial(6),"factorial")
+print(round(312.45636960, 5))
+print(max(2,4,5,1,2,7))
+print(min(2,4,5,1,2,7))"""
+
+# type convention -- 
+
+print(float(a))
+print(int(b))
+print(complex(2,3))
