fc7c9a2ecc95b6a8f3e9f3872422427c76abc9d6
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-26 21:05:52 +0600

    statement

---
+++ Statement.py
@@ -0,0 +1,17 @@
+# a = 20
+# b = 30
+# if a>=b:
+#     print("true hobe ans")
+# elif a<=b:
+#     print("fasle oho")
+
+
+# check grades 
+grade = 80
+
+if grade>70 or grade <= 82:
+    print("A+")
+elif grade>60 and grade>70:
+    print("A")
+else:
+    print("failed students")
