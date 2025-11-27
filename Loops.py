fea27896df104a595831ca23cc292868418f5baf
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-27 10:43:51 +0600

    completed loop

---
+++ Loops.py
@@ -0,0 +1,36 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+# for fruit in fruits:
+#     print(fruit)
+#     if fruit == "cherry":
+#         break
+
+"""word = "hello"
+for letter in word:
+    print(letter)"""
+
+"""dictionary = {"name": "Alice", "age": 30, "city": "New York"}
+for key in dictionary:
+    print(key," ",dictionary[key])"""
+
+# range function
+# for i in range(5):
+#     if(i==2):
+#         continue
+#     elif(i == 3):
+#         break
+#     print(i)
+
+""" WHILE LOOP """
+
+# while fruits:
+#     if(fruits[2]=="cherry"):
+#         break
+#     print(fruits)
+
+dictType = {"name": "Kazi", "age": 30, "city": "Feni"}
+keys = dictType.keys()
+index = 0
+while index < len(dictType[keys]):
+    print(dictType)
+    index += 1
+    
