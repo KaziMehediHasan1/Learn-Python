15ded081c42c577924f310a3ce5024d707badd7d
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-11-27 20:33:03 +0600

    completed set

--- Tuple.py
+++
@@ -1,4 +0,0 @@
-Tuple_One=("Kazi",24,24,"Feni")
-# print(Tuple_One.index(2))
-print(Tuple_One.count(24))
-print(Tuple_One)
---
+++ Tuple_Set.py
@@ -0,0 +1,13 @@
+Tuple_One=("Kazi",24,24,"Feni")
+# print(Tuple_One.index(2))
+print(Tuple_One.count(24))
+print(Tuple_One)
+# ---------- set -----------
+
+set_fruits={1,3,6}
+set_fruits_two={55,67,3,1}
+print(set_fruits.add("4"))
+
+# print(set_fruits.union(set_fruits_two)
+# )
+# print(set_fruits_two.intersection(set_fruits))
