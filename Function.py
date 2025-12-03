db642fee238b6e3d590aee1593323d312dbab5f6
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-12-03 18:24:40 +0600

    learn lambda,multi func

--- Function.py
+++ Function.py
@@ -1,23 +1,75 @@
-def check_func(name,age):
+"""def check_func(name,age):
     print("my name is", name, "and age is", age)
     
 check_func("Kazi",24)
-check_func("Kazi",24)
+check_func("Kazi",24)"""
 
 # tuple function
-def mult(*names):
+"""def mult(*names):
     return names
 
 ans = mult("kazi","mehedi","shakil","raju")
-print(list(ans))
+print(list(ans))"""
 
 # lambda
-secure = lambda x,y: x*y
-print(secure(3,4))
+"""secure = lambda x,y: x*y
+print(secure(3,4))"""
 
 # defualt value
-def sum(a,b=4):
+"""def sum(a,b=4):
     print(a+b)
     
-sum(30,100)
+sum(30,100)"""
+
+# variable length arguments
+"""def vla(*args):
+    for ag in args:
+        print(ag)
+        if(type(ag)=="str"):
+            print(ag)
     
+vla(45,60,"kazi",True,3.4)"""
+
+# dectionary
+"""def dict_fun(**args):
+    print(args["degree"])
+    
+dict_fun(
+    name="Kazi",
+    age=50,
+    degree="Diploma"
+)"""
+
+# return once
+"""def One_Return_Func(a,b=400):
+    sum = a+b
+    return sum
+
+result = One_Return_Func(30)
+print(result)"""
+
+# multifle return
+"""def calc(a,b=40):
+    sum = a+b,
+    sub= a-b,
+    if a % 2 == 0:
+        return "it's even"   
+    return sum,sub
+
+result = calc(34)
+print(result)"""
+
+"""def validate(pw):
+    if len(pw) < 6:
+        return False, "Password too short ❌"
+    return True, "Password OK ✔️"
+
+status, msg = validate("123456")
+print(status, msg)
+"""
+
+# lambda function
+"""result = lambda a,b:a+b;
+print(result(3,5))"""
+
+# 
